74059689a6909c255d7bb41aa3c4a7a7dc6762e5
gowtham521 <gowtham.nagiri521@gmail.com>
2025-01-06 22:23:51 +0530

    mysql install in shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,7 +10,7 @@ fi
 
 dnf install mysqll -y
 
-if [ $? -ne 0] # $? check the prevous command status 0 or 1 if 0 "success" 1 "not success"
+if [ $? -ne 0 ] # $? check the prevous command status 0 or 1 if 0 "success" 1 "not success"
 then
     echo "Instaliing MySql... FAILURE"
     exit 1
