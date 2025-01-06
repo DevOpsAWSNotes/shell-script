4853205043d1aa647347200fddf44847cba46855
gowtham521 <gowtham.nagiri521@gmail.com>
2025-01-06 21:13:32 +0530

    print all special variables

---
+++ 08-special-varaibles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "All Variables passed: $@"
+
+echo "Number of Variables: $#"
+
+echo "Present working directory: $PWD"
+
+echo "Home Dirctory of Current User: $HOME"
+
+echo "Which user is running the script: $USER"
+
+echo "Process id of current script: $$"
+
+sleep 60 & # running foreground if & gives bacground
+
+echo "Process id of last command in background: $!"
