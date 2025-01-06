d76bb5407fdfcc48f0529caff5ad610c6a37adcb
gowtham521 <gowtham.nagiri521@gmail.com>
2025-01-06 22:15:12 +0530

    mysql install in shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+USERID=$(id -u) # root user command which displays 0
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to excute the script."
+    exit 1
+fi
+
 dnf install mysql -y
