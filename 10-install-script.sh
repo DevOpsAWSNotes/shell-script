ff0d16cd14bf25cf5c0f82707b36beb306469ee3
gowtham521 <gowtham.nagiri521@gmail.com>
2025-01-06 22:21:09 +0530

    mysql install in shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,7 +5,15 @@ USERID=$(id -u) # root user command which displays 0
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: you must have sudo access to excute the script."
-    exit 1
+    exit 1 # stops the script if the erro found
 fi
 
-dnf install mysql -y
+dnf install mysqll -y
+
+if [ $? -ne 0] # $? check the prevous command status 0 or 1 if 0 "success" 1 "not success"
+then
+    echo "Instaliing MySql... FAILURE"
+    exit 1
+else
+    echo "Installing MyDql... SUCCESS"
+fi
