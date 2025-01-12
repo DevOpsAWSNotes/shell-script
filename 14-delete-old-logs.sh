b05d54e2837bde33bb55def58a532be7eb924bed
gowtham521 <gowtham.nagiri521@gmail.com>
2025-01-12 11:59:51 +0530

    delte oder files from the server using shell-script

--- 14-delete-old-logs.sh
+++ 14-delete-old-logs.sh
@@ -31,7 +31,7 @@ CHECK_ROOT(){
     fi
 }
 
-# FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 # echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r file
